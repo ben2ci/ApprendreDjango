c20aab101f5d1ee98d47ca710affc220c09b347f
ben2ci <ben2ci@yahoo.fr>
2025-07-08 16:41:34 +0000

    Creation des vues en django et routes URL

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,14 @@
+from http import HTTPStatus
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Meal
+
 
 # Create your views here.
+def index(request):
+    if request.method == "GET":
+        meals = Meal.objects.all()
+
+        return HttpResponse(meals)
+
+    return HttpResponse(HTTPStatus.BAD_REQUEST)
